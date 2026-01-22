2f12b3872931b663b81944c6ddcefd07f98d0bbb
Muhammad Ridwan <157098120+Muhd-Ridwan@users.noreply.github.com>
2026-01-22 12:22:51 +0800

    Adding a gameboard
    Adding a gameboard

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowPosition) => (
+        <li key={rowPosition}>
+          <ol>
+            {row.map((playerSymbol, colPost) => (
+              <li key={colPost}>
+                <button
+                  onClick={() => handleSelectSquare(rowPosition, colPost)}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
